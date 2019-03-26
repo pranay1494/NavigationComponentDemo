73e00127c5e65822360965bf32f33b56116d0e60
PranayBansal <pranaybansal@PranayBansals-MacBook-Air.local>
2019-03-26 13:00:46 +0530

    bug fix

--- app/src/main/java/com/example/navigationdemo/FragmentA.kt
+++ app/src/main/java/com/example/navigationdemo/FragmentA.kt
@@ -5,9 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_a.*
 
 class FragmentA : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_a,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        btn_c.setOnClickListener {view ->
+            view.findNavController().navigate(FragmentADirections.actionFragmentAToFragmentB())
+        }
     }
 }
--- app/src/main/java/com/example/navigationdemo/FragmentB.kt
+++ app/src/main/java/com/example/navigationdemo/FragmentB.kt
@@ -10,4 +10,9 @@ class FragmentB : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_b,container,false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
 }
